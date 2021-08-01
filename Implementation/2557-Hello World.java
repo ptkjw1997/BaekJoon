97cf48bc3a568e68a8ac9061b91fbcff49b56136
Jiwan <ptkjw1997@naver.com>
2021-08-01 17:02:30 +0900

    Solve 2557-Hello World

---
+++ Implementation/2557-Hello World.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+
+import java.io.*;
+
+
+public class Main {
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		System.out.println("Hello World!");
+		
+	}
+	
+}
+
