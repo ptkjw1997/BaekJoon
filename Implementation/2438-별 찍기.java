10691620e2dc6f47e8372cf4337a3bc0f0808d2f
Jiwan <ptkjw1997@naver.com>
2021-08-01 17:02:18 +0900

    Solve 2438-별 찍기

---
+++ Implementation/2438-별 찍기.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+
+import java.io.*;
+
+
+public class Main {
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < N; i++){
+			for(int j = 0; j<= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+	
+}
+
