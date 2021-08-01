0dee7259f9d6c73b38361b50316b031273ee89cd
Jiwan <ptkjw1997@naver.com>
2021-08-01 17:56:20 +0900

    Solve 11022-A+B-8

---
+++ Implementation/11022-A+B-8.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+
+import java.io.*;
+
+
+public class Main {
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		int N = Integer.parseInt(br.readLine());
+		int a, b;
+		String[] lines;
+		for(int i = 1; i <= N; i++) {
+			lines = br.readLine().split(" ");
+			a = Integer.parseInt(lines[0]);
+			b = Integer.parseInt(lines[1]);
+			System.out.println("Case #" + i +": " + a + " + " + b + " = " + (a+b));
+		}
+		
+	}
+	
+}
+
