d94b9755d32032c71eedc5a6ac19c4cb01f55191
Jiwan <ptkjw1997@naver.com>
2021-08-01 17:02:09 +0900

    Solve 10718-We love kriii

---
+++ Implementation/10718-We love kriii.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+
+import java.io.*;
+
+
+public class Main {
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		
+	}
+	
+}
+
