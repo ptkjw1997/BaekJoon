1b86518f9a2e3d1c66bfea3fb6b1033eb11d7b13
ptkjw1997 <ptkjw1997@naver.com>
2021-08-03 21:24:46 +0900

    Solve 2609-최대공약수와 최소공배수

---
+++ 2609-최대공약수와 최소공배수.java
@@ -0,0 +1,33 @@
+package Main;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String[] line = br.readLine().split(" ");
+		
+		int A = Integer.parseInt(line[0]), B = Integer.parseInt(line[1]);
+		System.out.println(get_gcd(A,B));
+		System.out.println(get_lcm(A,B));
+		
+	}
+	
+	private static int get_gcd(int A, int B) {
+		int max = Math.max(A, B);
+		int min = Math.min(A, B);
+		
+		if(max%min == 0) return min;
+		return get_gcd(max-min, min);
+	}
+	
+	private static int get_lcm(int A, int B) {
+		int gcd = get_gcd(A, B);
+		return A*B/gcd;
+	}
+}
